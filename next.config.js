a72680b7ceb028f1de134195a4929590c03345d1
Lucas Franzolin <lucasfranzolin@gmail.com>
2022-08-20 13:33:30 -0300

    next config ok

--- next.config.js
+++ next.config.js
@@ -1,15 +1,46 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: process.env.SERVICE_URL + '/:path*',
-            },
-        ];
-    },
-};
+/* eslint-disable @typescript-eslint/no-var-requires */
+const {
+    PHASE_DEVELOPMENT_SERVER,
+    PHASE_PRODUCTION_BUILD,
+} = require('next/constants');
 
-module.exports = nextConfig;
+module.exports = (phase) => {
+    const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+    const isProd =
+        phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1';
+
+    console.log(`phase: ${isDev ? 'dev' : isProd ? 'prod' : 'stg'}`);
+
+    const { serviceUrl } = (() => {
+        if (isDev)
+            return {
+                serviceUrl: 'http://localhost:4001',
+            };
+        if (isProd)
+            return {
+                serviceUrl: '',
+            };
+        return {
+            serviceUrl:
+                'https://70hlodrz9c.execute-api.us-east-1.amazonaws.com/stg',
+        };
+    })();
+
+    /**
+     * @type {import('next').NextConfig}
+     */
+    const nextConfig = {
+        reactStrictMode: true,
+        swcMinify: true,
+        async rewrites() {
+            return [
+                {
+                    source: '/api/:path*',
+                    destination: serviceUrl + '/:path*',
+                },
+            ];
+        },
+    };
+
+    return nextConfig;
+};
